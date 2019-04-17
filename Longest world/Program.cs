1b02c9fc744048143d3ab59be1b51245ef5ea2d7
visualstudio <visualstudio@122-PC26>
2019-04-17 10:01:16 -0400

    first commit

--- Longest world/Program.cs
+++ Longest world/Program.cs
@@ -10,6 +10,20 @@ namespace Longest_world
     {
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("the longest world is :");
+            Console.WriteLine(a.MyFunctionA("ada lovelace wrote the first algorith desingned for processing bt an Analytical Engine."));
+        }
+
+        public MyFunctionA(string input)
+        {
+            // Write a C# prograam to find the longest word in a string.
+            string[] words = input.Split('');
+
+            int wordArrayLength = words.Length;
+            int[] wordsLength = new int[wordArrayLength];
+            int x = 0;
+        }
         }
     }
 }
